b8757d2e826eb3c20c586f8862f04ff0b7fabb94
Marcos Tello <marcostello3596@gmail.com>
2024-06-10 17:51:35 -0300

    Fix routes

--- app/items/[id]/page.tsx
+++ app/items/[id]/page.tsx
@@ -1,24 +1,5 @@
+import api from '@/app/api';
 
 export default async function ItemPage({params: {id}}: {params: {id: string}}) {
-    const item = await fetch(
-        `https://api.mercadolibre.com/items/${id}`
-    ).then(
-        res =>
-            res.json() as Promise<{
-                id: string;
-                title: string;
-                thumbnail: string;
-                price: number;
-                currency_id: string;
-            }>,
-    );
-
-    const {plain_text} = await fetch(
-        `https://api.mercadolibre.com/items/${id}/description`
-    ).then(
-        res =>
-            res.json() as Promise<{
-                plain_text: string;
-            }>,
-    );
+    const item = await api.item.fetch(id);
 }
