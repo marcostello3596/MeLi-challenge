787fda629cea2aebbab1f07c5e8bac5c9a11f8ae
Marcos Tello <marcostello3596@gmail.com>
2024-06-10 18:25:01 -0300

    Add logo to the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body>
         <header className="h-16 bg-primary px-4 flex ">
-          <form action='/items' className="max-w-screen-xl gap-4 m-auto flex flex-1">
+          <form action='/items' className="max-w-screen-xl gap-4 m-auto flex flex-1 items-center">
+            <a href="/" className="flex items-center">
+              <img src="/mercadolibre.com.svg" alt="MercadoLibre Logo" className="h-8 mr-2" />
+            </a>
             <input name="search" type="text" className="h-8 flex-1 text-white px-2" />
             <button className="bg-lightGray px-2 py-1 h-8 text-slate-700" type="submit">Buscar</button>
           </form>
